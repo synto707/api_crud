503cdf7d6a073e0bdc539920133544a6b0183cc3
synto707 <ashok@syntorion.com>
2025-07-03 17:56:19 +0530

    Base Router

--- main.js
+++ main.js
@@ -3,12 +3,25 @@ import express from "express";
 const app = express();
 const port = 6969;
 
-app.get('/',(req,res)=>{
+//Crud Functions
+app.get('/movies',()=>{
 
-    res.json({msg:"Hello World"})
+    res.json({msg:"Hello World1"})
+})
+
+app.post('/movies',()=>{
+
+})
+
+app.put('/movies/:id',()=>{
+
+})
+
+app.delete('/movies/:id',()=>{
+
 })
 
 
 app.listen(port,()=>{
-    console.log(`Server is running on port  http://localhost:${port}`)
+    console.log(`the Server is running on port  http://localhost:${port}`)
 })
